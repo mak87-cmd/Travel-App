089045277541ef4588d857ae08805cbcab055560
Makenzie Cain <kenziecain87@gmail.com>
2021-05-23 20:49:16 -0500

    chore: update dev webpack config

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,12 +2,19 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-// const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
     mode: 'development',
     devtool: 'source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000,
+        headers: {
+            'Cache-Control': 'no-store'
+        }
+    },
     stats: 'verbose',
     output: {
         libraryTarget: 'var',
@@ -39,7 +46,6 @@ module.exports = {
             // Automatically remove all unused webpack assets on rebuild
             cleanStaleWebpackAssets: true,
             protectWebpackAssets: false
-        }),
-        // new WorkboxPlugin.GenerateSW()
+        })
     ]
 }
